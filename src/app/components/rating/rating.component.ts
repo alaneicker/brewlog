f90dd8928f004e31da998d2f0adcf77fec9a0f64
Alan Eicker <alaneicker@gmail.com>
2018-05-04 10:29:41 -0500

    dashboard

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -4,10 +4,10 @@ import { Component, OnInit, Input } from '@angular/core';
     selector: 'app-rating',
     styleUrls: ['./rating.component.scss'],
     template: `
-    <div class="c-rating">
-        <b>Overall Rating:</b>
-        <span class="u-text-gold" *ngFor="let star of stars">&#9733;</span>
-    </div>
+        <div class="c-rating">
+            Overall Rating:
+            <span class="u-text-gold" *ngFor="let star of stars">&#9733;</span>
+        </div>
     `,
 })
 export class RatingComponent implements OnInit {
