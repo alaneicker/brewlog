749063a01c0e14c481bb4e7fd756ed7af3d3b374
Alan Eicker <alaneicker@hotmail.com>
2018-05-08 11:49:28 -0500

    Update add-beer.module.ts

--- src/app/pages/add-beer/add-beer.module.ts
+++ src/app/pages/add-beer/add-beer.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AddBeerComponent } from './add-beer.component';
+import { AddBeerComponent }   from './add-beer.component';
+import { routing } from './add-beer.routing';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-    ],
-    providers: [],
-    declarations: [
-        AddBeerComponent,
-    ],
+  imports: [routing],
+  declarations: [AddBeerComponent]
 })
-
-export class AddBeerModule {}
+export class AdsBeerModule {}
