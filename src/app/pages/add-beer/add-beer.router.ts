c2609d97c248386c6d92843deebe658e14c0843f
Alan Eicker <alaneicker@hotmail.com>
2018-05-08 11:48:24 -0500

    Create add-beer.router.ts

---
+++ src/app/pages/add-beer/add-beer.router.ts
@@ -0,0 +1,8 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LazyComponent } from './lazy.component';
+
+const routes: Routes = [
+    { path: '', component: LazyComponent }
+];
