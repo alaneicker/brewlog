6b7100aa343e1a44317f5662004b8fe3efb71c0e
Alan Eicker <alaneicker@gmail.com>
2018-05-10 20:22:03 -0500

    google map

--- src/app/pages/beer-detail/beer-detail.module.ts
+++ src/app/pages/beer-detail/beer-detail.module.ts
@@ -7,6 +7,9 @@ import { BeerDetailResolver } from './beer-detail.resolver';
 import { BeerDetailComponent } from './beer-detail.component';
 import { SummarySectionComponent } from '../../components/summary-section/summary-section.component';
 import { RecommendedBeersComponent } from '../../components/recommended-beers/recommended-beers.component';
+import { GoogleMapComponent } from '../../components/google-map/google-map.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 import { routing } from './beer-detail.router';
 
@@ -15,11 +18,15 @@ import { routing } from './beer-detail.router';
         routing,
         CommonModule,
         RatingModule,
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyCmeCDLj1uvr97KxjUiaade1zfeya02Uog'
+        })
     ],
     declarations: [
         BeerDetailComponent,
         SummarySectionComponent,
         RecommendedBeersComponent,
+        GoogleMapComponent,
     ],
     providers: [
         BeerDetailResolver,
