c6b82f36351f81ede39161258cf50adf1d987bb6
Alan Eicker <alaneicker@gmail.com>
2018-05-11 09:40:14 -0500

    google maps

--- src/app/components/brewery-locations-map/brewery-locations-map.component.ts
+++ src/app/components/brewery-locations-map/brewery-locations-map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import * as geocoder from 'geocoder';
 
 export interface ILocation {
+    formattedAddress: string;
     lat: number;
     lng: number;
 }
@@ -15,7 +16,8 @@ export interface ILocation {
     `],
     template: `
         <div class="u-flex--medium u-flex--hr">
-            <div class="u-flex__item--grow u-vr-2" *ngFor="let l of locations">
+            <div class="u-flex__item--grow u-vr-1" *ngFor="let l of locations">
+                <h5 class="u-vr u-align-center">{{ l.formattedAddress }}</h5>
                 <agm-map [zoom]="zoom" [latitude]="l.lat" [longitude]="l.lng">
                     <agm-marker [latitude]="l.lat" [longitude]="l.lng"></agm-marker>
                 </agm-map>
@@ -24,20 +26,29 @@ export interface ILocation {
     `,
 })
 export class BreweryLocationMapsComponent implements OnInit {
-    @Input() location: string;
+    @Input() brewery: string;
+    @Input() city: string;
+    @Input() state: string;
+    @Input() country: string;
     @Input() zoom: number;
+
     locations: ILocation[] = [];
 
-    constructor() { }
+    constructor() {}
 
     ngOnInit() {
-        geocoder.geocode(this.location, ( err, data ) => {
+        geocoder.geocode(this.formattedLocationString, ( err, data ) => {
             data.results.forEach(location => {
                 this.locations.push({
+                    formattedAddress: location.formatted_address,
                     lat: location.geometry.location.lat,
                     lng: location.geometry.location.lng,
                 });
             });
         });
+    }
+
+    get formattedLocationString(): string {
+        return [this.brewery, this.city, this.state, this.country].join(', ');
     }
 }
