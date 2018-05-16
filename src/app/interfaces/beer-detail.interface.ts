5bdae46f9721e928203389027c23fd4ef2e4a384
Alan Eicker <alaneicker@gmail.com>
2018-05-16 18:14:37 -0500

    updated beer-detail interface

--- src/app/interfaces/beer-detail.interface.ts
+++ src/app/interfaces/beer-detail.interface.ts
@@ -1,14 +1,7 @@
 export interface IBeerDetail {
     id: number;
     beerName: string;
-    brewery: string;
     photoUrl?: string;
-    location?: string;
-    style?: string;
-    abv?: string;
-    ibu?: string;
-    glassware?: string;
     comments?: string;
     rating?: number;
-    url?: string;
 }
