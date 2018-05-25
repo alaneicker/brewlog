4971a559521f8200a4bc84cbd671eaa8d0853404
Alan Eicker <alaneicker@gmail.com>
2018-05-25 11:58:33 -0500

    focus trap for modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,26 +1,36 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef, OnChanges } from '@angular/core';
+
+const createFocusTrap = require('../../../../node_modules/focus-trap');
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
-  @Input() isOpen: boolean;
-  @Output() open: EventEmitter<any> = new EventEmitter();
-  @Output() close: EventEmitter<any> = new EventEmitter();
+export class ModalComponent implements OnInit, OnChanges {
+    @ViewChild('modal') modal: ElementRef;
+    @Input() isOpen: boolean;
+    @Output() close: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+    focusTrap: any;
 
-  ngOnInit() {
-  }
+    constructor() { }
 
-  handleOpen() {
-    this.open.emit();
-  }
+    ngOnInit() {
+    }
 
-  handleClose() {
-    this.close.emit();
-  }
+    ngOnChanges() {
+        if (this.isOpen) {
+            setTimeout(() => {
+                this.focusTrap = createFocusTrap('#modal', {});
+                this.focusTrap.activate();
+            }, 500);
+        }
+    }
+
+    handleClose() {
+        this.close.emit();
+        this.focusTrap.deactivate();
+    }
 
 }
