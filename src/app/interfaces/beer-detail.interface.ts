70b729c9f41509f736c7d002e750ebe8a152109a
Alan Eicker <alaneicker@gmail.com>
2018-05-29 20:53:14 -0500

    updated IBeerDetail interface

--- src/app/interfaces/beer-detail.interface.ts
+++ src/app/interfaces/beer-detail.interface.ts
@@ -2,8 +2,9 @@ export interface IBeerDetail {
     routeId: string;
     id: number;
     beerName: string;
+    datePosted: string;
     photoUrl?: string;
-    comments?: string;
-    rating?: string;
-    imgDataUri?: string;
+    comments: string;
+    rating: string;
+    imgDataUri: string;
 }
