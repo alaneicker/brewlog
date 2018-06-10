6e98d075363d20df937eb5d5b28f02b63f925e3a
Alan Eicker <alaneicker@gmail.com>
2018-06-10 07:57:18 -0500

    Fixes rating not updating after summary section update

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'app-rating',
@@ -9,13 +9,17 @@ import { Component, OnInit, Input } from '@angular/core';
         </div>
     `,
 })
-export class RatingComponent implements OnInit {
+export class RatingComponent implements OnInit, OnChanges {
     @Input() rating: number;
     stars: number[];
 
     constructor() { }
 
     ngOnInit() {
+        this.stars = Array(this.rating).fill(0);
+    }
+
+    ngOnChanges() {
         this.stars = Array(this.rating).fill(0);
     }
 
